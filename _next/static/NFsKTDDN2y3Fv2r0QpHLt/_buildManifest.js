9c7a83068c6ff464f0d277b2282f97b1169d9dad
edmhar <edmhar@nexusgaming.tv>
2020-10-23 16:01:31 +0800

    Deploy Next.js to gh-pages

--- _next/static/NFsKTDDN2y3Fv2r0QpHLt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fe68b2c94319242f8e688.css","static\u002Fchunks\u002Fpages\u002Findex-c5f8617a677abd4853f3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fb942535921b5be64a37.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bf2ce6b53d9555573f3e";
+/******/ 	var hotCurrentHash = "0d2e34d97bdc7f7b37dd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
