8126818287c6f80b31e3314aa283f615c929379e
edmhar <edmhar@nexusgaming.tv>
2020-10-23 11:03:25 +0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/mkQlvvZEHa4quI_rlLi-o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fe68b2c94319242f8e688.css","static\u002Fchunks\u002Fpages\u002Findex-7562adfe6bab4e045e64.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fb942535921b5be64a37.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
