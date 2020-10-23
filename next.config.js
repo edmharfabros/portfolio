28631935d87957c4ead8fa99eb66ae2d5012575c
edmhar <edmhar@nexusgaming.tv>
2020-10-23 09:26:20 +0800

    Deploy Next.js to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
-
 module.exports = {
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: isProd ? '/portfolio' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -14,11 +9,10 @@ module.exports = {
       },
       use: ['@svgr/webpack'],
     });
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      }),
-    )
     return config;
   },
+  assetPrefix: isProd ? '/portfolio' : '',
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
 };
