7f92f9187d86e68dd5a4f26d4a86fb8053c3ad89
edmhar <edmhar@nexusgaming.tv>
2020-10-23 09:15:23 +0800

    Initial out subtree commit gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+
 module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+  assetPrefix: isProd ? '/portfolio' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -7,6 +14,11 @@ module.exports = {
       },
       use: ['@svgr/webpack'],
     });
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+      }),
+    )
     return config;
   },
 };
